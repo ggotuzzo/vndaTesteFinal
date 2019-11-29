98c133af5372315ae894e538c407cc377282a071
ggotuzzo <58018876+ggotuzzo@users.noreply.github.com>
2019-11-28 23:21:43 -0300

    Add files via upload

---
+++ dist/home.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VNDA</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-compatible" content="IE-Edge">
+	<link rel="stylesheet" type="text/css" href="scss/style.css">
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.css"/> 
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.css"/> 
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
+	<meta name="description" content="descrição do site">
+	<meta name="keyword" content="palavras chaves">
+	<meta name="author" content="criador da pagina">
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+</head>
+<body>
+	<div class="fill_top"></div><!--fill-top-->
+	<header>
+		<div class="container">
+			<nav>
+				<h3><i class="fas fa-bars"></i></h3>
+				<ul class="center">
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+					<li><a href="#">Link 4</a></li>
+					<li><a href="#">Link 5</a></li>
+				</ul>
+			</nav>
+			<div class="logo">
+				<h1 class="center">VNDA</h1>
+			</div><!---->
+			<div class="icon_top">
+				<i id="login_top" class="fas fa-user-alt"></i>
+				<i class="fas fa-shopping-bag"></i>
+			</div><!--icon_top-->
+			<div class="clear"></div><!--clear-->			
+		</div><!--container-->
+		<div class="conceitual">			
+			<div class="wrapper_conceitual">
+				<img src="https://picsum.photos/id/1080/6858/4574"/>
+				<div class="bg_conceitual">
+					<h1>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using </h1>
+					<div><a href="#">Saiba Mais</a></div>
+				</div><!--bg_conceitual-->
+			</div><!--wrapper_conceitual-->
+			<div class="wrapper_conceitual">
+				<img src="https://picsum.photos/id/309/3872/2592"/>
+				<div class="bg_conceitual">
+					<h1>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using </h1>
+					<div><a href="#">Saiba Mais</a></div>
+				</div><!--bg_conceitual-->
+			</div><!--wrapper_conceitual-->
+		</div><!--conceitual-->
+	</header>
+	<section class="text_one">
+		<div class="container">
+			<h2>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has </h2>
+		</div><!--container-->
+	</section><!--text_one-->
+	<section class="sessao_produtos">
+		<div class="container">
+			<h2 class="center">Produtos Lorem Ipsum</h2>
+		</div><!--container-->
+		<div class="produtos">
+				<div class="wrapper_produtos">
+					<h4>Suco Tropical</h4>
+					<div class="line_produtos bg_rose"></div><!--line_produtos-->
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s...</p>
+					<img src="img/img1_produto.png">
+					<a href="#">Saiba mais <i class="fas fa-arrow-right"></i></a>
+					<div class="line_right bg_rose"></div>
+				</div><!--wrapper_produtos-->		
+				<div class="wrapper_produtos">
+					<h4>Suco Tropical</h4>
+					<div class="line_produtos bg_rose"></div><!--line_produtos-->
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s...</p>
+					<img src="img/img1_produto.png">
+					<a href="#">Saiba mais <i class="fas fa-arrow-right"></i></a>
+					<div class="line_right bg_rose"></div>
+				</div><!--wrapper_produtos-->		
+				<div class="wrapper_produtos">
+					<h4>Suco Tropical</h4>
+					<div class="line_produtos bg_rose"></div><!--line_produtos-->
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s...</p>
+					<img src="img/img1_produto.png">
+					<a href="#">Saiba mais <i class="fas fa-arrow-right"></i></a>
+				</div><!--wrapper_produtos-->		
+			<div class="clear"></div><!--clear-->
+		</div><!--produtos-->
+	</section><!--sessao_produtos-->
+	<section class="sessao_produtos_destaques">
+		<div id="select_produtos" class="container">
+			<div class="produtos_destaques_wrapper">
+				<img src="img/destaque_img.png" alt=""/>
+				<p>It is a long established fact that a reader will be distracted by the readable content of a page...</p>
+				<a href="#">Saiba mais</a>
+			</div><!--produtos_destaques_wrapper-->
+			<div class="clear"></div><!--clear-->
+		</div><!--container-->
+	</section><!--sessao_produtos_destaques-->
+	<section class="map" id="mapa">
+	</section><!--map-->
+	<footer class="footer_one">
+		<div class="container">
+			<div class="wrapper_footer">
+				<h4>Endereço - Contato</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipis aliqua. Ut enimunit mets delore <br/>+55 51 3333-3333 <u> seuemail@seuemail.com.br</u></p>
+			</div><!--wrapper_footer-->
+			<div class="wrapper_footer center">
+				<h1 class="center">VNDA</h1>
+				<a href="#"><i class="fab fa-facebook-f"></i></a>
+				<a href="#"><i class="fab fa-twitter"></i></a>
+				<a href="#"><i class="fab fa-instagram"></i></a>
+			</div><!--wrapper_footer-->
+			<div class="wrapper_footer">
+				<h4>Newsletter</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipis aliqua. Ut enimunit mets delore </p>
+				<form class="news_footer">
+					<input type="text" name="email" placeholder="Adicione seu e-mail"><input type="button" name="acao" value="ENVIAR">
+				</form><!--news_footer-->
+			</div><!--wrapper_footer-->
+		</div><!--container-->
+	</footer><!--footer_one-->
+	<footer class="footer_two center">
+		<p>Todos os direitos reservados a VNDA © 2019</p>
+	</footer><!--footer_two-->
+</body>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.js"></script>
+<script src="js/function.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnv7y3NuVB3H7-6XXXbtFUYOsvWql_d-8&"></script>
+<script src="https://kit.fontawesome.com/fe15543e03.js"></script>
+</html>
